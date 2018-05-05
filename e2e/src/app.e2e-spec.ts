339b4536bcaa3f50b5348a85cbadcaaee053ffe2
Tiep Phan <tieppt12@gmail.com>
2018-05-05 19:33:10 +0700

    feat(e2e): fix bug

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('pt-training App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toContain('App Component');
   });
 });
