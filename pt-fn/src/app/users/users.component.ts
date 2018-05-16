666ad9df80514d27fc9ae59969c07128e9792dd2
Tiep Phan <tieppt12@gmail.com>
2018-05-16 20:01:07 +0700

    feat(core): step 1

--- pt-fn/src/app/users/users.component.ts
+++ pt-fn/src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  userList: any[] = [];
+  loading = false;
 
-  constructor() { }
+  postByUser: any[] = [];
+  showPost = false;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.http.get('https://jsonplaceholder.typicode.com/users')
+      .pipe(
+        delay(1000)
+      )
+      .subscribe((next: any[]) => {
+        this.userList = next;
+      }, error => {
+        console.log(error);
+        this.loading = false;
+      }, () => {
+        this.loading = false;
+      });
+  }
+
+  fetchPostByUserId(userId) {
+    console.log(userId);
+    this.http.get('https://jsonplaceholder.typicode.com/posts', {
+      // params: new HttpParams().set('userId', userId)
+      params: {
+        userId: userId
+      }
+    })
+    .subscribe((data: any[]) => {
+      this.postByUser = data;
+      this.showPost = true;
+    }, err => {
+      this.postByUser = [];
+      this.showPost = false;
+    });
   }
 
 }
