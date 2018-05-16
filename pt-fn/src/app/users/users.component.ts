451760d2cad2688cf1d35e6538f331aea85e7b33
Tiep Phan <tieppt12@gmail.com>
2018-05-16 20:42:51 +0700

    feat(core): http post

--- pt-fn/src/app/users/users.component.ts
+++ pt-fn/src/app/users/users.component.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { delay } from 'rxjs/operators';
 
 @Component({
@@ -14,9 +15,27 @@ export class UsersComponent implements OnInit {
   postByUser: any[] = [];
   showPost = false;
 
-  constructor(private http: HttpClient) { }
+  userForm: FormGroup;
+
+  postSuccess = false;
+  postResponse: any;
+
+  get nameCtrl() {
+    return this.userForm.get('name');
+  }
+
+  get usernameCtrl() {
+    return this.userForm.get('username');
+  }
+
+  constructor(private http: HttpClient, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.email]
+    });
     this.loading = true;
     this.http.get('https://jsonplaceholder.typicode.com/users')
       .pipe(
@@ -47,6 +66,20 @@ export class UsersComponent implements OnInit {
       this.postByUser = [];
       this.showPost = false;
     });
+  }
+
+  onSubmit() {
+    console.log(this.userForm.value);
+
+    if (this.userForm.valid) {
+      this.http.post('https://jsonplaceholder.typicode.com/users',
+        this.userForm.value
+      ).subscribe(res => {
+        this.postSuccess = true;
+        this.postResponse = res;
+      }, err => this.postSuccess = false);
+
+    }
   }
 
 }
